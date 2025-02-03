f3ad159b51e307202da65e63dd7de3f57e7f04d8
Sgroleonel <Sgroleo9@gmail.com>
2025-02-03 20:53:38 -0300

    solving jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',            // Tells Jest to use ts-jest preset for TypeScript
-  testEnvironment: 'node',      // Jest environment for Node.js (can also be 'jsdom' for browser-like tests)
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      isolatedModules: true,     // Speeds up test compilation by not type-checking
-    }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],  // Add TypeScript extensions
-  testMatch: [
-    '**/spec/**/*.spec.ts',    // Adjust this to your folder structure for test files
-  ],
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',         // Adjust alias for @src
-    '^spec/support/(.*)$': '<rootDir>/spec/support/$1',  // Map spec/support
-  },
-  roots: ['<rootDir>/src', '<rootDir>/spec'],  // Ensure Jest starts in these directories
-};
-
+    preset: 'ts-jest',            // Tells Jest to use ts-jest preset for TypeScript
+    testEnvironment: 'node',      // Jest environment for Node.js (can also be 'jsdom' for browser-like tests)
+    transform: {
+      '^.+\\.(ts|tsx)$': ['ts-jest', {
+        isolatedModules: true,     // Speeds up test compilation by not type-checking
+      }],
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],  // Add TypeScript extensions
+    testMatch: [
+      '**/spec/**/*.spec.ts',    // Adjust this to your folder structure for test files
+    ],
+    moduleNameMapper: {
+      '^@src/(.*)$': '<rootDir>\\src\\$1',  // Usar \\ en lugar de / para Windows
+      '^spec/support/(.*)$': '<rootDir>\\spec\\support\\$1',
+    }
+  };
   
