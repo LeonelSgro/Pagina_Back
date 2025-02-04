e04b01da0d69390711a30a25b0f22609cc55ccc1
Sgroleonel <Sgroleo9@gmail.com>
2025-02-03 21:36:57 -0300

    jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,26 @@
+/*
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      isolatedModules: true,
-    }],
+  transform: {'^.+\\.(ts|tsx)$': ['ts-jest', { isolatedModules: true,}],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
   testMatch: [
+    '**///spec/**/*.spec.ts',
+ // ],
+  //moduleNameMapper: {
+   // '^@src/(.*)$': '<rootDir>/src/$1', // Usa <rootDir> para referenciar la raíz del proyecto
+  //},
+//};
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1', // Mapeo para @src
+    '^spec/(.*)$': '<rootDir>/spec/$1', // Mapeo para spec
+  },
+  testMatch: [
     '**/spec/**/*.spec.ts',
   ],
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1', // Usa <rootDir> para referenciar la raíz del proyecto
-  },
 };
