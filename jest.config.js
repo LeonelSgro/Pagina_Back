1cfd2f3c769e0903a1c5e37672b9ce8c2c401867
Sgroleonel <Sgroleo9@gmail.com>
2025-02-03 21:19:29 -0300

    test jest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    preset: 'ts-jest',            // Tells Jest to use ts-jest preset for TypeScript
-    testEnvironment: 'node',      // Jest environment for Node.js (can also be 'jsdom' for browser-like tests)
-    transform: {
-      '^.+\\.(ts|tsx)$': ['ts-jest', {
-        isolatedModules: true,     // Speeds up test compilation by not type-checking
-      }],
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],  // Add TypeScript extensions
-    testMatch: [
-      '**/spec/**/*.spec.ts',    // Adjust this to your folder structure for test files
-    ],
-      "moduleNameMapper": {
-        "/^@src\/(.*)$/": "/home/runner/work/Pagina_Back/Pagina_Back/src/$1"
-    }
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      isolatedModules: true,
+    }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  testMatch: [
+    '**/spec/**/*.spec.ts',
+  ],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1', // Usa <rootDir> para referenciar la raíz del proyecto
+  },
+};
