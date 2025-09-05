65e16b01dc3699b60cd9ef8116aa30364834b2df
Teo418 <teoibanham@gmail.com>
2025-09-05 11:06:53 -0300

    cambios_server

--- src/server.ts
+++ src/server.ts
@@ -33,9 +33,18 @@ if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
 }
 
 // Security
-if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
-  app.use(helmet());
-}
+if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {}
+
+app.use((req: Request, res: Response, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next(); // Don't forget to call next() to pass control to the next middleware
+});
+
 
 // Add APIs, must be after middleware
 app.use(Paths.Base, BaseRouter);
